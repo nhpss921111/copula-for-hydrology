2e56c5477006f661a2549018a5711d96df07328b
Job Lien <nhpss921111@gamil.com>
2020-06-14 00:26:53 +0800

    放棄lgamma3 新增weibull gamma3]未完成

--- 20200613_easymethod.R
+++ 20200613_easymethod.R
@@ -30,6 +30,7 @@ library("lestat") #inverse CDF
 library("goftest") #適合度檢定
 library("goft") #適合度檢定
 library("gumbel")
+library(PearsonDS)
 
 
 # Read data from excel flie
@@ -61,20 +62,22 @@ S <- rm.data$S
 # ------------------------------------------------------------------------------------
 
 print("參數估計")
-candidate <- c("norm","lnorm","gumbel","gamma3","lgamma3")
+candidate <- c("norm","lnorm","gumbel","weibull","gamma3")
 
 dgumbel <- function(x, a, b) 1/b*exp((a-x)/b)*exp(-exp((a-x)/b))
 pgumbel <- function(q, a, b) exp(-exp((a-q)/b))
 qgumbel <- function(p, a, b) a-b*log(-log(p))
 
 variable <- cbind(Q,S)
-dist.table <- matrix(nrow=length(candidate)+1,ncol=dim(variable))
-rownames(dist.table) <- c("norm","lnorm","gumbel","gamma3","lgamma3","good dist")
+dist.table <- matrix(nrow=length(candidate)+1,ncol=dim(variable)[2])
+rownames(dist.table) <- c("norm","lnorm","gumbel","weibull","gamma3","good dist")
 
 
 
 for(i in 1:dim(variable)[2]){
   var <- variable[,i]
+  if (i==1){print("Q")}
+  if (i==2){print("S")}
   # By Maximun Likelihood Estimate Method
   for(dist in c(1:length(candidate))){
     # -------------------------- parameter estimate -----------------------
@@ -100,33 +103,36 @@ for(i in 1:dim(variable)[2]){
       par1 <- md$parameters[1] #fitting參數1
       par2 <- md$parameters[2] #參數2
       print(c(par1, par2))
+    }else if(candidate[dist] == "weibull"){
+      md <- fitdist(var, dist = dist.char[1])
+      par1 <- md$estimate[1] #fitting參數1
+      par2 <- md$estimate[2] #參數2
+      print(c(par1, par2))
     }else if(candidate[dist] == "gamma3"){
-      prefit(var,"gamma3","mle",list(shape=1,scale=1,thres=1),lower=-Inf, upper=Inf)
-      md <- fitdist(var, dist = dist.char[1], start=list(shape=1,scale=1,thres=0.1),
-                    lower = c(0,0,0),upper=c(Inf,Inf,min(var)),control=list(trace=1, REPORT=1))
+      #prefit(var,"gamma3","mle",list(shape=0.1,scale=200,thres=1),lower=0, upper=Inf)
+      md <- fitdist(var, dist = dist.char[1], start=list(shape=1,scale=50,thres=1),
+                    lower = c(0,0,0),upper=c(Inf,Inf,min(var))) #control=list(trace=1, REPORT=1)
       par1 <- md$estimate[1] #fitting參數1
       par2 <- md$estimate[2] #參數2
       par3 <- md$estimate[3] #參數3
       print(c(par1, par2, par3))
       #plot(md)
-    }else if(candidate[dist] == "lgamma3"){
-      md <- fitdist(var, dist = dist.char[1],start=list(shape=0.1,scale=0.1,thres=0.1),lower=c(0.1,0.1,0.1),
-                    control=list(trace=1, REPORT=1))
-      par1 <- md$estimate[1] #fitting參數1
-      par2 <- md$estimate[2] #參數2
-      par3 <- md$estimate[3] #參數3
-      print(c(par1, par2, par3))
+    # }else if(candidate[dist] == "lgamma3"){
+    #   md <- fitdist(var, dist = dist.char[1],start=list(shape=10,scale=10,thres=1)
+    #                 ,lower=c(0.1,0.1,0.1),upper=c(Inf,Inf,min(var)))
+    #   par1 <- md$estimate[1] #fitting參數1
+    #   par2 <- md$estimate[2] #參數2
+    #   par3 <- md$estimate[3] #參數3
+    #   print(c(par1, par2, par3))
       #plot(md)
     }else{break}
-    
-    
-    
     
     # ------------------------------- K-S test ----------------------
     print("KS test")
     if(candidate[dist] == "norm"){xi.cdf <- get(dist.char[3])(var, par1,par2)}
     if(candidate[dist] == "lnorm"){xi.cdf <- get(dist.char[3])(var, meanlog = par1, sdlog = par2)}
     if(candidate[dist] == "gumbel"){xi.cdf <- get(dist.char[3])(var, par1, par2)}
+    if(candidate[dist] == "weibull"){xi.cdf <- get(dist.char[3])(var, par1, par2)}
     if(candidate[dist] == "gamma3"){xi.cdf <- get(dist.char[3])(var, shape=par1, scale=par2, thres=par3)}
     if(candidate[dist] == "lgamma3"){xi.cdf <- get(dist.char[3])(var, shape=par1, scale=par2, thres=par3)}
     
@@ -144,51 +150,3 @@ for(i in 1:dim(variable)[2]){
   
 }
 
-
-
-# weibull distribution
-Q.par.weibull   <- fitdist(Q, distr = "weibull", method = "mle") # Q parameter
-Q.shape.weibull <- Q.par.weibull[["estimate"]][["shape"]]
-Q.scale.weibull <- Q.par.weibull[["estimate"]][["scale"]]
-S.par.weibull   <- fitdist(S, distr = "weibull", method = "mle",lower=c(0, 0))# s parameter
-S.shape.weibull <- S.par.weibull[["estimate"]][["shape"]]
-S.scale.weibull <- S.par.weibull[["estimate"]][["scale"]]
-## gamma distribution
-Q.par.gamma   <- fitdist(Q, distr = "gamma", method = "mle",lower=c(0, 0)) # Q parameter
-Q.shape.gamma <- Q.par.gamma[["estimate"]][["shape"]]
-Q.rate.gamma  <- Q.par.gamma[["estimate"]][["rate"]]
-S.par.gamma   <- fitdist(S, distr = "gamma", method = "mle",lower=c(0, 0))# s parameter
-S.shape.gamma <- S.par.gamma[["estimate"]][["shape"]]
-S.rate.gamma  <- S.par.gamma[["estimate"]][["rate"]]
-## log-normal distribution
-Q.par.lnorm       <- fitdist(Q,"lnorm",method = "mle") # Q parameter
-Q.meanlog.lnorm   <- Q.par.lnorm[["estimate"]][["meanlog"]]
-Q.sdlog.lnorm     <- Q.par.lnorm[["estimate"]][["sdlog"]]
-S.par.lnorm       <- fitdist(S,"lnorm",method = "mle") # S parameter
-S.meanlog.lnorm   <- S.par.lnorm[["estimate"]][["meanlog"]]
-S.sdlog.lnorm     <- S.par.lnorm[["estimate"]][["sdlog"]]
-# pearson tpye three distribution
-# Q.par.lgamma      <- fitdist(Q,"lgamma",method = "mle") # Q parameter
-# Q.shapelog.lgamma <- Q.par.lgamma[["estimate"]][["shapelog"]]
-# Q.ratelog.lgamma  <- Q.par.lgamma[["estimate"]][["ratelog"]]
-# S.par.lgamma      <- fitdist(S,"lgamma",method = "mle") # S parameter
-# S.shapelog.lgamma <- S.par.lgamma[["estimate"]][["shapelog"]]
-# S.ratelog.lgamma  <- S.par.lgamma[["estimate"]][["ratelog"]]
-# gumbel distribution
-Q.par.gumbel      <- eevd(Q, method = "mle") #EnvStats # Q parameter
-Q.location.gumbel <- Q.par.gumbel[["parameters"]][["location"]]
-Q.scale.gumbel    <- Q.par.gumbel[["parameters"]][["scale"]]
-S.par.gumbel      <- eevd(S, method = "mle") #EnvStats # S parameter
-S.location.gumbel <- S.par.gumbel[["parameters"]][["location"]]
-S.scale.gumbel    <- S.par.gumbel[["parameters"]][["scale"]]
-
-plot(Q.par.weibull)
-plot(Q.par.gamma)
-plot(Q.par.lnorm)
-#plot(Q.par.lgamma)
-
-
-plot(S.par.weibull)
-plot(S.par.gamma)
-plot(S.par.lnorm)
-#plot(S.par.lgamma)
