a3236cdb155ca4225dc644e399b81ae53cef871d
Job Lien <nhpss921111@gamil.com>
2020-06-29 21:43:15 +0800

    更新到2019年

--- txt2xlsx.R
+++ txt2xlsx.R
@@ -7,12 +7,13 @@
 
 rm(list = ls()) # 清除資料
 library(xlsx)
-setwd("E:/R_reading/suspend")
-year <- c(98:107) # 請輸入年分：
+setwd("E:/R_reading/CHIA-YUANG/suspend")
+year <- c(98:108) # 請輸入年分：
 input <- c(paste0(year,".txt")) 
 output <- c(paste0(year,".xlsx")) 
 for(i in c(1:length(input))){ 
   data <- read.table(file.path(getwd(),input[i])) #按年份讀取資料
+  # 資料解析
   sus1 <- data[,1:6] #把資料分成sus1和sus2
   sus2 <- data[,7:12]
   colnames(sus2) <- c(paste0("V",1:6))
@@ -21,6 +22,6 @@ for(i in c(1:length(input))){
   sus.table <- sus.table[complete.cases(sus.table), ] #移除na值
   colnames(sus.table) <- c("No.","Month","Day","Discharge",
                          "Sediment Content","Suspended Load") #新增欄位名稱
-  file <- paste("E:/R_output/", output[i], sep="") #存檔路徑
+  file <- paste("E:/R_output/CHIA-YUANG/", output[i], sep="") #存檔路徑
   write.xlsx(sus.table,file) #結果寫到xlsx裡面
 }
