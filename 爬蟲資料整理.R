8f56dabae5b619799d616383fa254a203a263f94
Job Lien <nhpss921111@gamil.com>
2020-07-06 22:35:57 +0800

    輸砂量資料

--- 爬蟲資料整理.R
+++ 爬蟲資料整理.R
@@ -1,27 +1,32 @@
 # 爬蟲資料整理
 # 開始撰寫日期：2020/07/05
-# 完成撰寫日期：2020/07/05
+# 完成撰寫日期：2020/07/06
 #
 rm(list=ls())
 library(dplyr)
-setwd("E:/R_reading/CHIA-YUANG")
-data <- read.csv(file.path(getwd(),"2560H017.csv"), header = T)
-data <- data[,-1]
-data[1,2:7]
-year <- c(1974:2019) # 請輸入年分：
-
-new.data <- c()
+# 參數給定----
+year <- c(1974:2000) # 請輸入年分：
+input.file <- c(paste0(year,".csv"))
+# -----------
+setwd("E:/R_reading/CHIA-YUANG/") # 讀取檔案之路徑
 
 for (n in c(1:length(year))){
-  ini.data <- data[n,2:7]
-  colnames(ini.data) <- c("No.","Month","Day","Q","Sc","Sl")
+  data <- read.csv(file.path(getwd(),input.file[n]), header = T) #按年份讀取資料
+  data <- data[,-1]
+  ini.data <- data[2:7]
+  arrange.data <- c()
   i <- 1
-  while (i <145 ){
-    slice.data <- data[n,(i+1):(i+6)]
-    colnames(slice.data) <- c("No.","Month","Day","Q","Sc","Sl")
-    new.data <- rbind(new.data,slice.data)
+  while (i <300){
+    slice.data <- data[(i+1):(i+6)] #每一天的資料
+    arrange.data <- rbind(arrange.data,slice.data)
+    colnames(arrange.data) <- c("No.","Month","Day","Discharge",
+                                "Sediment Content","Suspended Load")
     i <- i+6
-    new.data <- arrange(new.data,No.) #依照No.欄位，由小到大排序
-    
   }
+  rm.data <- arrange.data[complete.cases(arrange.data), ]
+  new.data <- arrange(as.data.frame(rm.data),No.) #依照No.欄位，由小到大排序
+  new.data[,1] <- year[n]
+  colnames(new.data) <- c("Year","Month","Day","Discharge",
+                          "Sediment Content","Suspended Load")
+  write.csv(new.data, file = paste0("E:/R_output/CHIA-YUANG/",year[n], ".csv"))
 }
