0e052defcf25badefb4b04dbefa8203201a23f62
Job Lien <nhpss921111@gamil.com>
2020-07-06 22:22:06 +0800

    資料一年存一筆

--- crawling.R
+++ crawling.R
@@ -1,14 +1,16 @@
 # 來源：爬蟲，水文年報(2000年以前)  2019/11/02 BY奕廷學長
-# 
+# 因為每年觀測次數不同，所以無法一次全部爬完再存
+# 所以需要每爬完一年就儲存
 # 開始撰寫日期：2020/07/02
-# 完成撰寫日期：2020/07/02
+# 完成撰寫日期：2020/07/06
 rm(list = ls())
 library(rvest)
-output <- "n"
+output <- "y"
 #
 #----起始資料
 #測站編號
 station <- c("2560H017")
+year <- c(1974:2000)
 #
 #----
 # 先建立錯誤函數：
@@ -38,27 +40,23 @@ daily.rec.err.func <- function(x){
 
 #抓取每年資料
 for(s in c(1:length(station))){
-  all.data <- c(NULL)
-  
-  for(yr in c(1974:2000)){ #資料年分
+  for(yr in c(1974:2000)){ #資料年分：
+    year.data <- c()
     url <- paste0("https://gweb.wra.gov.tw/wrhygis/ebooks/ebook/ebook/hyb", yr, "/", station[s], ".HTML")
-     #get.rec.err.func(
-    data <- read_html(url) %>% html_nodes(".Section1 td font") %>% html_text
-    #)
+    get.rec.err.func(
+      data <- read_html(url) %>% html_nodes(".Section1 td font") %>% html_text
+    )
     t = 1
     while(t < length (data)){
       data[t] <- daily.rec.err.func(as.numeric(data[t]))
       t = t + 1
     }
-    
-    all.data <- rbind(all.data, c(yr, data))
-    break
-    
-  }
-  #輸出csv
-  #all.data <- all.data[,-2:-18]
-  if(output == "y"){
-    write.csv(all.data, file = paste0("E:/R_output/crawling/",station[s], ".csv")) 
+    year.data <- rbind(year.data, c(yr, data))
+    year.data <- year.data[-2:-18] # 刪除多餘欄位
+    year.data <- as.data.frame(year.data)
+    #輸出csv
+    if(output == "y"){
+      write.csv(year.data, file = paste0("E:/R_output/crawling/CHIA-YUANG/",yr, ".csv"))}
   }
 }
 
