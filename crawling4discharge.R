d1d13bfdb216a81fb20dcf4de49e38bceed12342
Job Lien <nhpss921111@gmail.com>
2020-12-04 17:17:44 +0800

    爬取流量資料

---
+++ crawling4discharge.R
@@ -0,0 +1,77 @@
+# 來源：爬蟲，水文年報(2000年以前)  2019/11/02 BY奕廷學長
+# 因為每年觀測次數不同，所以無法一次全部爬完再存
+# 所以需要每爬完一年就儲存
+# 開始撰寫日期：2020/07/02
+# 完成撰寫日期：2020/07/06
+rm(list = ls())
+library(rvest)
+library(magrittr)
+library(jsonlite)
+output <- "y"
+#
+#----起始資料
+#測站編號
+station <- c("2560H017")
+year <- c(1974:2000)
+#
+#----
+# 先建立錯誤函數：
+# 1.空白值將錯誤，建立tryCatch將空白或缺測值轉為0
+get.rec.err.func<- function(x){
+  tryCatch({x},
+           warning = function(msg){
+             return(matrix(0, nrow = 1, ncol = 600))# 回傳0值
+           },
+           error = function(msg){
+             return(matrix(0, nrow = 1, ncol = 600))# 回傳0值
+           }
+  )
+}
+# 2.測站缺測時值都是0
+daily.rec.err.func <- function(x){
+  tryCatch({x},
+           warning = function(msg){
+             return(0)
+           },
+           error = function(msg){
+             return(0)
+           }
+  )
+}
+#----
+sum.data <- c()
+#抓取每年資料
+for(s in c(1:length(station))){
+  
+  for(yr in year){ #資料年分：
+    url <- paste0("https://gweb.wra.gov.tw/wrhygis/ebooks/ebook/ebook/hyb", yr, "/", station[s], ".HTM")
+    get.rec.err.func(
+      data <- read_html(url) %>% html_nodes(xpath = "//td") %>% html_text()
+    )
+    t = 1
+    while(t < length (data)){
+      data[t] <- daily.rec.err.func(as.numeric(data[t]))
+      t = t + 1
+    }
+    sum.data <- cbind(sum.data, c(yr, data))
+    sum.data <- as.data.frame(sum.data)
+  } 
+}
+rm.data <- sum.data[28:430,] # 移除多餘的欄位
+colnames(rm.data) <- year # 列的名稱
+
+for (i in 1:length(year)){
+  year.line <- t(data.frame(rm.data[paste0(year[i])])) #選出一年的資料
+  
+  year.table <- t(data.frame(matrix(year.line,ncol=31,nrow=13))) # 將數列資料轉成(月*日)資料
+  
+  year.data<- as.data.frame(year.table) # (月*日)資料轉成 data.frame
+  year.data <- cbind(year[i],year.data)
+  colnames(year.data) <- c("年","日","一月","二月","三月","四月","五月","六月",
+                           "七月","八月","九月","十月","十一","十二月")
+
+  #輸出csv
+  if(output == "y"){
+    write.csv(year.data, file = paste0("F:/R_output/crawling/CHIA-YUANG/discharge/",year[i], ".csv"))}
+}
+
