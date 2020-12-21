184e08f8de5a6448b2d0a57597408d98f0fb358b
Job Lien <nhpss921111@gmail.com>
2020-12-21 16:19:06 +0800

    整理爬蟲抓下來的Q及Qs

--- 爬蟲資料整理.R
+++ 爬蟲資料整理.R
@@ -5,10 +5,10 @@
 rm(list=ls())
 library(dplyr)
 # 參數給定----
-year <- c(1974:2000) # 請輸入年分：
+year <- c(1987:2000) # 請輸入年分：
 input.file <- c(paste0(year,".csv"))
 # -----------
-setwd("E:/R_reading/CHIA-YUANG/") # 讀取檔案之路徑
+setwd("F:/R_reading/CHUNYUN BRIDGE/crawling/SSL") # 讀取檔案之路徑
 
 for (n in c(1:length(year))){
   data <- read.csv(file.path(getwd(),input.file[n]), header = T) #按年份讀取資料
@@ -28,5 +28,5 @@ for (n in c(1:length(year))){
   new.data[,1] <- year[n]
   colnames(new.data) <- c("Year","Month","Day","Discharge",
                           "Sediment Content","Suspended Load")
-  write.csv(new.data, file = paste0("E:/R_output/CHIA-YUANG/",year[n], ".csv"))
+  write.csv(new.data, file = paste0("F:/R_output/CHUNYUN BRIDGE/",year[n], ".csv"))
 }
