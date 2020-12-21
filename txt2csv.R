401e831f2633e28129edf30e7b2920aedd537d3b
Job Lien <nhpss921111@gmail.com>
2020-12-21 17:27:24 +0800

    完成

--- txt2csv.R
+++ txt2csv.R
@@ -2,13 +2,14 @@
 # from：txt2xlsx
 # txt轉換成csv
 # 撰寫日期：2020/07/06
-# 完成日期：2020/07/06
+# 完成日期：2020/12/21
 # 事前準備：每年的輸砂量與流量
+# 更改路徑就能做不同的測站
 # Input："年分".txt
 # Ouput："年分".csv
 
 rm(list = ls()) # 清除資料
-setwd("E:/R_reading/CHIA-YUANG/suspend")
+setwd("F:/R_reading/CHUNYUN BRIDGE/form_pdf/SSL/") #濁水溪-彰雲橋
 year <- c(2001:2019) # 請輸入年分：
 input <- c(paste0(year,".txt")) 
 output <- c(paste0(year,".csv")) 
@@ -24,6 +25,6 @@ for(i in c(1:length(input))){
   sus.table[,1] <- year[i]
   colnames(sus.table) <- c("Year","Month","Day","Discharge",
                          "Sediment Content","Suspended Load") #新增欄位名稱
-  file <- paste("E:/R_output/CHIA-YUANG/", output[i], sep="") #存檔路徑
-  write.csv(sus.table,file) #結果寫到xlsx裡面
+  file <- paste("F:/R_output/CHUNYUN BRIDGE/SSL_arranged/", output[i], sep="") #存檔路徑
+  write.csv(sus.table,file) #結果寫到csv裡面
 }
