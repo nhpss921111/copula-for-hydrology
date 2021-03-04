b7edbb42457f58f23fdf83f23d4a75b892913baa
Job Lien <nhpss921111@gmail.com>
2021-03-04 16:39:08 +0800

    add內茅埔

--- 爬蟲資料整理.R
+++ 爬蟲資料整理.R
@@ -1,14 +1,30 @@
 # 爬蟲資料整理
+# 整理同時有流量和輸砂量的資料(使用crawling.R爬下來的資料)
 # 開始撰寫日期：2020/07/05
-# 完成撰寫日期：2020/07/06
+# 完成撰寫日期：2021/03/04
+# =============================================
+# 更改路徑就能做不同的測站
+#station <- c("CHIA-YUANG")
+#station.num <- c("2560H017") #蘭陽溪-家源橋
+#year <- c(1974:2000) # 1974才開始有SSL的紀錄
 #
+#station <- c("CHUNYUN BRIDGE")
+#station.num <- c("1510H057") #濁水溪-彰雲橋
+#year <- c(1987:2000) # 1987才開始有SSL的紀錄
+#
+#station <- c("NEI-MAO-PU")
+#station.num <- c("1510H049") #濁水溪-內茅埔
+#year <- c(1972:2000) # 1972才開始有SSL的紀錄
+# =============================================
 rm(list=ls())
 library(dplyr)
 # 參數給定----
-year <- c(1987:2000) # 請輸入年分：
+station <- c("NEI-MAO-PU")
+year <- c(1972:2000) # 請輸入年分：
 input.file <- c(paste0(year,".csv"))
 # -----------
-setwd("F:/R_reading/CHUNYUN BRIDGE/crawling/SSL") # 讀取檔案之路徑
+setwd(paste0("F:/R_output/crawling/",station,"/discharge+SSL")) # 讀取檔案之路徑
+
 
 for (n in c(1:length(year))){
   data <- read.csv(file.path(getwd(),input.file[n]), header = T) #按年份讀取資料
@@ -28,5 +44,5 @@ for (n in c(1:length(year))){
   new.data[,1] <- year[n]
   colnames(new.data) <- c("Year","Month","Day","Discharge",
                           "Sediment Content","Suspended Load")
-  write.csv(new.data, file = paste0("F:/R_output/CHUNYUN BRIDGE/",year[n], ".csv"))
+  write.csv(new.data, file = paste0("F:/R_output/",station,"/SSL_arranged/",year[n], ".csv"))
 }
