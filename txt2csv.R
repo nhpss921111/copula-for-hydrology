42f217a8fa0dda355c34282c693dbe9d3527d3b8
Job Lien <nhpss921111@gmail.com>
2021-03-04 16:11:57 +0800

    add內茅埔

--- txt2csv.R
+++ txt2csv.R
@@ -2,22 +2,29 @@
 # from：txt2xlsx
 # txt轉換成csv
 # 撰寫日期：2020/07/06
-# 完成日期：2020/12/21
+# 完成日期：2021/03/04
 # 事前準備：每年的輸砂量與流量
 # Input："年分".txt
 # Ouput："年分".csv
-
+# ========================================
 # 更改路徑就能做不同的測站
-# CHIA-YUANG：家源橋
-# CHUNYUN BRIDGE：彰雲橋
-
+#station <- c("CHIA-YUANG") #家源橋
+#year <- c(2001:2019) 
+#
+#station <- c("CHUNYUN BRIDGE") #彰雲橋
+#year <- c(2001:2019) 
+#
+#station <- c("NEI-MAO-PU") #內茅埔
+#year <- c(2001,2003:2019) 
+# ========================================
 rm(list = ls()) # 清除資料
-setwd("F:/R_reading/CHUNYUN BRIDGE/form_pdf/SSL/") #濁水溪-彰雲橋
-year <- c(2001:2019) # 請輸入年分：
+station <- c("NEI-MAO-PU")
+year <- c(2001,2003:2019) # 請輸入年分：
+setwd(paste0("F:/R_reading/",station,"/from_pdf/discharge+SSL/")) 
 input <- c(paste0(year,".txt")) 
 output <- c(paste0(year,".csv")) 
 for(i in c(1:length(input))){ 
-  data <- read.table(file.path(getwd(),input[i])) #按年份讀取資料
+  data <- read.table(file.path(getwd(),input[i]),fill=TRUE) #按年份讀取資料
   # 資料解析
   sus1 <- data[,1:6] #把資料分成sus1和sus2
   sus2 <- data[,7:12]
@@ -28,6 +35,6 @@ for(i in c(1:length(input))){
   sus.table[,1] <- year[i]
   colnames(sus.table) <- c("Year","Month","Day","Discharge",
                          "Sediment Content","Suspended Load") #新增欄位名稱
-  file <- paste("F:/R_output/CHUNYUN BRIDGE/SSL_arranged/", output[i], sep="") #存檔路徑
+  file <- paste(paste0("F:/R_output/",station,"/SSL_arranged/", output[i], sep="")) #存檔路徑
   write.csv(sus.table,file) #結果寫到csv裡面
 }
